13e8523783f3d578ba07d159c06ffa986657b858
Salim Fadhley <sal@stodge.org>
2014-04-11 23:49:16 +0100

    New stuff

---
+++ setup.py
@@ -0,0 +1,44 @@
+from setuptools import setup
+import os
+
+PROJECT_ROOT, _ = os.path.split(__file__)
+REVISION = '0.0.1'
+PROJECT_NAME = 'ZQTable'
+PROJECT_AUTHORS = "Salim Fadhley"
+# Please see readme.rst for a complete list of contributors
+PROJECT_EMAILS = 'salimfadhley@gmail.com'
+PROJECT_URL = "https://bitbucket.org/salimfadhley/zqtable"
+SHORT_DESCRIPTION = 'Tabular and time-sequence data in pure python'
+
+try:
+    DESCRIPTION = open(os.path.join(PROJECT_ROOT, "README.rst")).read()
+except IOError, _:
+    DESCRIPTION = SHORT_DESCRIPTION
+
+
+setup(
+    name=PROJECT_NAME.lower(),
+    version=REVISION,
+    author=PROJECT_AUTHORS,
+    author_email=PROJECT_EMAILS,
+    packages=['zqtable', 'zqtable_tests'],
+    zip_safe=True,
+    include_package_data=False,
+    install_requires=['requests>=1.2.3', 'pytz>=2013b'],
+    test_suite='nose.collector',
+    tests_require=['mock', 'nose', 'coverage'],
+    url=PROJECT_URL,
+    description=SHORT_DESCRIPTION,
+    long_description=DESCRIPTION,
+    license='MIT',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Software Development :: Testing',
+    ],
+)
