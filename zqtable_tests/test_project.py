0fc3f42c9391e65f173a708330368f227eac9879
Salim Fadhley <sal@stodge.org>
2014-04-15 23:50:10 +0100

    add the missing project tests

---
+++ zqtable_tests/test_project.py
@@ -0,0 +1,44 @@
+import unittest
+from zqtable import Table
+
+class TestProject(unittest.TestCase):
+
+    def setUp(self):
+        self.s = [
+            ('A', int),
+            ('B', float),
+            ('C', str),
+        ]
+        self.t = Table(self.s)
+
+        self.t.extend([
+            [1, 1.1, 'hello'],
+            [2, 2.2, 'yello'],
+        ])
+
+    def test_simple_project(self):
+        t = self.t.project(['A', 'C'])
+        self.assertEqual(t.schema, [('A', int), ('C', str)])
+        self.assertEqual(t[0], (1, 'hello'))
+
+    def test_anti_project(self):
+        t = self.t.anti_project(['B'])
+        self.assertEqual(t.schema, [('A', int), ('C', str)])
+        self.assertEqual(t[0], (1, 'hello'))
+
+    def test_anti_project_multiple_arguments(self):
+        t = self.t.anti_project('A', 'B')
+        self.assertEqual(t.schema, [('C', str)])
+        self.assertEqual(t[0], ('hello',))
+
+    def test_expand_const_on_project(self):
+        t0 = self.t.project(['C'])
+        t1 = t0.expand_const('D', 0)
+        self.assertEqual(
+            list(t1.D),
+            [0] * len(self.t)
+        )
+
+
+if __name__ == '__main__':
+    main()
