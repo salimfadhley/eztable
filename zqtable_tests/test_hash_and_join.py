7352058909333c1036571ad69e7a62a96c3fe44c
Salim Fadhley <sal@stodge.org>
2014-04-15 10:01:24 +0100

    added some stuff

---
+++ zqtable_tests/test_hash_and_join.py
@@ -0,0 +1,29 @@
+import unittest
+from zqtable import Table
+
+
+class TestHashAndJoin(unittest.TestCase):
+
+    def setUp(self):
+        self.s = [
+            ('A', int),
+            ('B', float),
+            ('C', str),
+            ('D', tuple),
+        ]
+
+        self.t = Table(self.s)
+        self.t.extend([
+            [1, 1.1, 'a', (1, 2)],
+            [2, 1.2, 'b', (2, 3)],
+            [3, 1.3, 'c', (3, 4)],
+        ])
+
+    # def test_simple_hash(self):
+    #     t = self.t.hash('H', ['C'])
+
+    #     self.assertEquals(
+    #         t.H.to_list(),
+    #         [hash(('a')), hash(('b')), hash(('c'))]
+
+    #     )
