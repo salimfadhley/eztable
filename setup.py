d78d79a7342f7a70f728ac4185e34503c6d6fff1
Salim Fadhley <sal@stodge.org>
2014-04-17 23:37:18 +0100

    version bump

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 import os
 
 PROJECT_ROOT, _ = os.path.split(__file__)
-REVISION = '0.0.11'
+REVISION = '0.0.12'
 PROJECT_NAME = 'ZQTable'
 PROJECT_AUTHORS = "Salim Fadhley"
 # Please see readme.rst for a complete list of contributors
