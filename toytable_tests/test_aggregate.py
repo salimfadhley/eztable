dac4b658e1e73265b217536a0120b45b31e2e888
Salim Fadhley <sal@stodge.org>
2014-04-22 21:33:29 +0100

    added agg tests

---
+++ toytable_tests/test_aggregate.py
@@ -0,0 +1,23 @@
+import unittest
+from toytable import table_literal
+
+class TestAggregate(unittest.TestCase):
+    """Verify the correctness of aggegation functions.
+    """
+    def setUp(self):
+        self.t = table_literal("""
+            | Attack (str)  | Pokemomn (str) | Level Obtained (int) | Attack Type (str) |
+            | Thunder Shock | Pikachu        | 1                    | Electric          |
+            | Tackle        | Pikachu        | 1                    | Normal            |
+            | Tail Whip     | Pikachu        | 1                    | Normal            |
+            | Growl         | Pikachu        | 5                    | Normal            |
+            | Quick Attack  | Pikachu        | 10                   | Normal            |
+            | Thunder Wave  | Pikachu        | 13                   | Electric          |
+            | Electro Ball  | Pikachu        | 18                   | Electric          |
+            | Charm         | Pikachu        | 0                    | Fairy             |
+            | Sweet Kiss    | Pikachu        | 0                    | Fairy             |
+        """)
+
+    def test_simple_aggregate(self):
+	pass
+
