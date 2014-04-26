5ba4bf05963594fc82a9de461bd156ea137a8409
Salim Fadhley <sal@stodge.org>
2014-04-26 23:30:34 +0100

    added an example of how to do aggregation

---
+++ toytable_examples/example_aggregation.py
@@ -0,0 +1,22 @@
+from toytable import table_literal
+t = table_literal("""
+| Attack(str)   | Pokemon(str) | Level Obtained(int) | Attack Type(str) |
+| Thunder Shock | Pikachu      | 1                   | Electric         |
+| Tackle        | Pikachu      | 1                   | Normal           |
+| Tail Whip     | Pikachu      | 1                   | Normal           |
+| Growl         | Pikachu      | 5                   | Normal           |
+| Quick Attack  | Pikachu      | 10                  | Normal           |
+| Thunder Wave  | Pikachu      | 13                  | Electric         |
+| Electro Ball  | Pikachu      | 18                  | Electric         |
+| Charm         | Pikachu      | 0                   | Fairy            |
+| Sweet Kiss    | Pikachu      | 0                   | Fairy            |
+""")
+
+agg = t.aggregate(
+    keys=('Pokemon', 'Attack Type'),
+    aggregations = [
+        ('Count', int, lambda t:len(t))
+    ]
+)
+
+print agg
