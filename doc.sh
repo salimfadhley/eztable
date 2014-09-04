0c3bc2ff61b298e39156f8bc1628568c6b533cbc
salimfadhley <salim.fadhley@gmail.com>
2014-09-05 00:50:00 +0100

    tweaks to doc script - currently has to be run from python 2.7

--- doc.sh
+++ doc.sh
@@ -1,12 +1,9 @@
 #! /bin/bash
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-cd $DIR/src
-source setup.sh
-pip install sphinx
 cd $DIR/src/doc
 make clean
 make html
-cd $DIR
+cd $DIR/src
 python setup.py upload_docs
 cd $DIR
